868bc903ecf731ceb7e88497a3155b0ad1e43ae5
karenrivgi <karenrivgi@gmail.com>
2024-03-21 10:48:45 -0500

    Generacion de archivos

---
+++ preguntas.py
@@ -0,0 +1,30 @@
+import os.path
+import pandas as pd
+
+def generar_csv(path):
+    '''Genera un archivo csv con los datos de las frases y su 
+    sentimiento en el path especificado'''
+
+    data = {"phrase": [], "sentiment": []}
+
+    # Recorrer los archivos de texto y guardar los datos
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(".txt"):
+                with open(os.path.join(root, file), "r") as f:
+                    phrase = f.read()
+                    data["phrase"].append(phrase)
+
+                    sentiment = os.path.basename(root)
+                    data["sentiment"].append(sentiment)
+    
+    # Crear el dataframe y guardarlo en un archivo csv
+    df = pd.DataFrame(data)
+    name = path.replace("data/", "")
+    df.to_csv(f"{name}_dataset.csv", index=False)
+
+if __name__ == "__main__":
+    generar_csv("data/train")
+    generar_csv("data/test")
+
+        
